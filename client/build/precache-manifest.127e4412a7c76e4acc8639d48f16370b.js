d94b93ffa9cdc15698d15e89858f1b91d88608bb
DanielSkoros <morsmorfie@gmail.com>
2019-11-17 19:35:16 +0100

    add build

---
+++ client/build/precache-manifest.127e4412a7c76e4acc8639d48f16370b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5a79739f4e6206d4ec0be540898571",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c923ebfeafbfd0d878ab",
+    "url": "/static/css/main.ea54ea8d.chunk.css"
+  },
+  {
+    "revision": "3c59f7072ee71c3ddc3a",
+    "url": "/static/js/2.9b29f939.chunk.js"
+  },
+  {
+    "revision": "c923ebfeafbfd0d878ab",
+    "url": "/static/js/main.24930a90.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
